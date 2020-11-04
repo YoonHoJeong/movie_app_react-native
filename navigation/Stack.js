9604e919dede4a3c236ca36812d454a8d54ae11a
Jeong YoonHo <uno8941@gmail.com>
2020-11-04 22:06:19 +0900

    header, navigation done

---
+++ navigation/Stack.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Tabs from "../navigation/Tabs";
+import Detail from "../screens/Detail";
+
+const Stack = createStackNavigator();
+
+export default () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tabs" component={Tabs} />
+    <Stack.Screen name="Detail" component={Detail} />
+  </Stack.Navigator>
+);
